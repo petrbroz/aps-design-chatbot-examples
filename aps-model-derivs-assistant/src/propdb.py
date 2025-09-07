7f915fa99e60f6a8392c655befb60884501c5442
Petr Broz <petr.broz@autodesk.com>
2025-09-07 23:30:44 +0200

    Added missing unit.

--- aps-model-derivs-assistant/src/propdb.py
+++ aps-model-derivs-assistant/src/propdb.py
@@ -44,6 +44,7 @@ def _parse_length(value):
         "km": 1000,
         "in": 0.0254,
         "ft": 0.3048,
+        "fractional-in": 0.3048,
         "ft-and-fractional-in": 0.3048,
         "yd": 0.9144,
         "mi": 1609.34
